d07e4c0faaf7cbeb80be4be19795e0e2d6605fd6
Robert Shiple <robert.shiple@gmail.com>
2017-09-01 16:26:56 -0700

    more changes

--- gardenbotvr/views.py
+++ gardenbotvr/views.py
@@ -4,11 +4,6 @@ import random
 from .models import ThreeD
 # Create your views here.
 def home(request):
-    alligator = [33343329399393, 'sonsonson']
-    tiger = 'defefef'
-    dog = 5
-    now = datetime.now()
-    choice = random.choice(alligator)
     colladas = ThreeD.objects.all()
-    context = {'todd': alligator, 'jerry': tiger, 20: dog, 'today': now, 'plants': choice, 'colladas': colladas}
+    context = {'colladas': colladas}
     return render(request, 'index.html', context)
